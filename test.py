dec8cfa4c0e8d89b32788d201abc2f03d61ef3d7
Keitaro Natori <ghq0826@gmail.com>
2020-04-01 11:09:53 +0900

    first commit

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print("a")
+D=[]
+E=[]
+with open("input.txt") as input:
+    for line in input:
+        line=line.strip()
+        if  str(line).isdecimal()==False:
+            D.append(line)
+        else:
+            num=int(line)
+for i in D:
+    a,b=i.split(":")
+    E.append((int(a),b))
+E=sorted(E)
+words=[]
+for i in range(len(E)):
+    if num%E[i][0]==0:
+        words.append(E[i][1])
+word="".join(words)
+print(word)
